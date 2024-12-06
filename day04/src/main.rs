908eb5421ebf65eb2285f1f5392ac4e596063e81
Martin Laubscher <martin.laubscher@me.com>
2024-12-06 18:14:02 +0100

    day 04 part 01

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,59 +1,69 @@
 use std::fs;
-use std::cmp;
-use std::cmp::Ordering;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct Location(i8, i8);
+#[derive(Clone, Debug, PartialEq)]
+struct Direction(i8, i8);
 
-impl Location {
-    fn r#move(mut self, direction: (i8, i8)) { 
-        self.0 += direction.0;
-        self.1 += direction.1;
-    }
-}
-
+const ALL_DIRECTIONS: &[Direction] = &[
+    Direction(-1, 0),  // UP
+    Direction(1, 0),   // DOWN
+    Direction(0, -1),  // LEFT
+    Direction(0, 1),   // RIGHT
+    Direction(-1, -1), // UP_LEFT
+    Direction(-1, 1),  // UP_RIGHT
+    Direction(1, -1),  // DOWN_LEFT
+    Direction(1, 1),   // DOWN_RIGHT
+];
 
 fn main() {
     let file_path = "day04/input.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let lines: Vec<Vec<char>> = contents.lines().map(|l| l.chars().collect()).collect();
-    println!("{:?}", lines);
-    
+    let mut result = 0;
+
     let rows = lines.len();
     let cols = lines[0].len();
-    
+
     for row in 0..rows {
-        let x = rows;
         for col in 0..cols {
             if lines[row][col] == 'X' {
-                let x = rows;
-                let mut stack = vec!['S', 'A', 'M'];
-                match ((row as i64-1).cmp(&0), 
-                       (col as i64-1).cmp(&0), 
-                       (row+1).cmp(&(rows - 1)), 
-                       (col+1).cmp(&(cols - 1))) 
-                {
-                    // top left
-                    (Ordering::Less, Ordering::Less, _, _) => {},
-                    // top right
-                    (Ordering::Less, _, _, Ordering::Greater) => {},
-                    // bottom left
-                    (_, Ordering::Less, Ordering::Greater, _) => {},
-                    // bottom right
-                    (_, _, Ordering::Greater, Ordering::Greater) => {},
-                    // top row
-                    (Ordering::Less, _, _, _) => {},
-                    // bottom row
-                    (_, _, Ordering::Greater, _) => {},
-                    // leftmost col
-                    (_, Ordering::Less, _, _) => {},
-                    // rightmost column
-                    (_, _, _,Ordering::Greater) => {},
-                    // all other
-                    _ => {}
+                let allowed = allowed_directions(row, col, rows, cols);
+
+                for direction in allowed {
+                    let mut stack: Vec<char> = vec!['S', 'A', 'M'];
+                    let mut new_row = row as i64 + direction.0 as i64;
+                    let mut new_col = col as i64 + direction.1 as i64;
+                    let mut valid = true;
+                    
+                    while !stack.is_empty()
+                        && valid
+                    {
+                        valid = lines[new_row as usize][new_col as usize] == stack.pop().unwrap();
+                        if allowed_directions(new_row as usize, new_col as usize, rows, cols)
+                            .contains(&direction) {
+                            new_row += direction.0 as i64;
+                            new_col += direction.1 as i64;
+                        } else {
+                            break;
+                        }
+                    }
+                    if stack.is_empty() && valid {
+                        result += 1;
+                    }
                 }
-                stack.pop();
             }
         }
     }
+    println!("Part 2: {:?}", result);
+}
+
+fn allowed_directions(row: usize, col: usize, rows: usize, cols: usize) -> Vec<Direction> {
+    ALL_DIRECTIONS
+        .iter()
+        .filter(|&&Direction(r_off, c_off)| {
+            let new_r = row as isize + r_off as isize;
+            let new_c = col as isize + c_off as isize;
+            new_r >= 0 && new_r < rows as isize && new_c >= 0 && new_c < cols as isize
+        })
+        .cloned()
+        .collect()
 }
